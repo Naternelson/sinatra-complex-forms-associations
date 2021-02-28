f8f2fd7c51a8196f8e5af0e7982213b5e3b885e7
Nate <7nater.nelson@gmail.com>
2021-02-27 21:18:24 -0700

    commit tree aa29dc5dbb7729008cfbd32f7e913043f2892167

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,30 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(name: params[:pet_name])
+    if !params[:owner_name].empty?
+      owner = Owner.create(name: params[:owner_name])
+      owner.pets << @pet
+    else
+      owner = Owner.find_by(id: params[:owner])
+      owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find_by(id: params[:id])
+    if !@pet
+      redirect to '/pets'
+    end
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +37,21 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
+     ####### bug fix
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+    end
+      #######
 
+    @pet = Pet.find(params[:id])
+    @pet.name = params["pet_name"]
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.save
+    else
+      @pet.owner = Owner.find(params[:pet][:owner_id])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
